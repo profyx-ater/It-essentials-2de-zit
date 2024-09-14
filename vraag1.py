c55af9d538e6cffe4d2cd46116f0dbb98c4739c4
profyx-ater <65975213+profyx-ater@users.noreply.github.com>
2024-09-14 17:59:48 +0200

    vragen

--- vraag1.py
+++ vraag1.py
@@ -1,7 +1,101 @@
+# naam: Robbe Profeta
+# klas: 1 TINH
+import random
+
 
 def main():
+    aantal_verjaardagen = int(input("Hoeveel verjaardagen wil je genereeren? (max. 100) "))
+    while aantal_verjaardagen < 0 or aantal_verjaardagen > 100:
+        aantal_verjaardagen = int(input("Hoeveel verjaardagen wil je genereeren? (max. 100) "))
+
     maanden = ['Januari;31', 'Februari;29', 'Maart;31', 'April;30', 'Mei;31', 'Juni;30',
-               'Juli;31','Augustus;31', 'September;30', 'Oktober;31', 'November;30', 'December;31']
+               'Juli;31', 'Augustus;31', 'September;30', 'Oktober;31', 'November;30', 'December;31']
+    genereerVerjaardagen(maanden, aantal_verjaardagen)
+
+
+def genereerVerjaardagen(maanden, aantal_verjaardagen):
+    maandNaam = delenMaanden(maanden)
+    maandAantalDagen = delenDagen(maanden)
+    genereerdeMaand = []
+    genereerdeDag = []
+    for i in range(aantal_verjaardagen):
+        random_maand = random.randint(1, len(maandNaam))
+        genereerdeMaand.append(maandNaam[random_maand - 1])
+        hoeveel_dagen_in_maand = maandAantalDagen[random_maand - 1]
+        random_dag = random.randint(1, int(hoeveel_dagen_in_maand))
+        genereerdeDag.append(random_dag)
+    verjaardagen = PlaatsVerjaardagenOpScherm(genereerdeMaand, genereerdeDag)
+    aantalVerjaardagenPerMaand(genereerdeMaand, maandNaam)
+    verjaardagFrequentie(verjaardagen)
+
+
+def verjaardagFrequentie(verjaardagen):
+    verjaardagenAlGepaseerd = []
+    aantalkeerGepaseerd = []
+    for i in range(len(verjaardagen)):
+        for j in range(len(verjaardagen)):
+            if verjaardagenAlGepaseerd.count(verjaardagen[i]) == 0:
+                verjaardagenAlGepaseerd.append(verjaardagen[i])
+                aantalkeerGepaseerd.append(1)
+            else:
+                plaats = verjaardagenAlGepaseerd.index(verjaardagen[i])
+                tijdelijkgetal = aantalkeerGepaseerd[plaats]
+                tijdelijkgetal += 1
+
+    indexHoogstedag = 0
+    hoogstegetal = 0
+    aantalgepaseerd = 0
+    for x in range(len(aantalkeerGepaseerd)):
+        if int(aantalkeerGepaseerd[x]) > hoogstegetal:
+            indexHoogstedag = x
+            aantalgepaseerd = aantalkeerGepaseerd[x]
+    print(f"de verjaardag die het vaakst voorkomt is: {verjaardagenAlGepaseerd[indexHoogstedag]}")
+    print(f"Deze verjaardag komt {aantalgepaseerd} keer voor.")
+
+
+def aantalVerjaardagenPerMaand(genereerdeMaand, maandNaam):
+    print("Aaantal per maand:")
+    aantalVerjaardagenPerMaandLijst = []
+    for lijstVullen in range(12):
+        aantalVerjaardagenPerMaandLijst.append(0)
+    for i in range(len(genereerdeMaand)):
+        for j in range(len(maandNaam)):
+            if genereerdeMaand[i] == maandNaam[j]:
+                tijdelijkAantal = aantalVerjaardagenPerMaandLijst[j]
+                tijdelijkAantal += 1
+                aantalVerjaardagenPerMaandLijst[j] = tijdelijkAantal
+
+    for x in range(len(maandNaam)):
+        aantalVerjaardagen = aantalVerjaardagenPerMaandLijst[x]
+        sterren = ""
+        for j in range(aantalVerjaardagen):
+            sterren += "*"
+        print(f"{maandNaam[x]} {sterren}")
+
+
+def PlaatsVerjaardagenOpScherm(genereerdeMaand, genereerdeDag):
+    print("De genereerde verjaardagen zijn: ")
+    verjaardagen = []
+    for i in range(len(genereerdeMaand)):
+        verjaardagen.append(f"{genereerdeDag[i]} {genereerdeMaand[i].lower()[0:3]}")
+        print(f"{genereerdeDag[i]} {genereerdeMaand[i].lower()[0:3]}")
+    return verjaardagen
+
+
+def delenMaanden(maanden):
+    lijstNaam = []
+    for i in range(len(maanden)):
+        maandNaam = maanden[i].split(";")
+        lijstNaam.append(maandNaam[0])
+    return lijstNaam
+
+
+def delenDagen(maanden):
+    lijstDagen = []
+    for i in range(len(maanden)):
+        maandFagen = maanden[i].split(";")
+        lijstDagen.append(maandFagen[1])
+    return lijstDagen
 
 
 if __name__ == '__main__':
--- vraag2.py
+++ vraag2.py
@@ -1,29 +1,93 @@
-# def selecteer_activiteiten():
+# naam: Robbe Profeta
+# klas: 1 TINH
+def selecteer_activiteiten(activiteiten, naam):
+    naamLetters = []
+    mogelijkActiviteiten = []
+    naamLetters.append(naam[0:1].lower())
+    naamLetters.append(naam[len(naam) - 1:len(naam)].lower())
+    even = 0
+    middelsteletter = ""
+    if len(naam) % 2 != 0:
+        middelsteletter = naam[len(naam) // 2]
+        even = 1
+    for i in range(len(activiteiten)):
+        hetWoord = []
+        for letter in activiteiten[i]:
+            hetWoord.append(letter.lower())
+        if hetWoord.count(naamLetters[0]) != 0 and hetWoord.count(naamLetters[1]) != 0:
+            if even == 1:
+                if hetWoord.count(middelsteletter) == 0:
+                    mogelijkActiviteiten.append(activiteiten[i])
+            elif even == 0:
+                mogelijkActiviteiten.append(activiteiten[i])
+
+    return mogelijkActiviteiten
 
 
-
-# def bepaal_prijs_geselecteerde_activiteiten():
-
+def bepaal_prijs_geselecteerde_activiteiten(geslecteerdeActiviteit, code):
+    cijfers = []
+    OmgezetteCode = str(code)
+    cijfers.append(OmgezetteCode[1:2])
+    cijfers.append(OmgezetteCode[3:4])
+    volledigecijfer = cijfers[0] + cijfers[1]
+    volledigecijferNaarInt = int(volledigecijfer)
+    prijs = []
+    for i in range(len(geslecteerdeActiviteit)):
+        GratisActiviteit = 0
+        for letter in geslecteerdeActiviteit[i].upper():
+            if ord(letter) == volledigecijferNaarInt:
+                GratisActiviteit = 1
+            elif ord(letter.lower()) == volledigecijferNaarInt:
+                GratisActiviteit = 1
+        if GratisActiviteit == 1:
+            prijs.append(0)
+        else:
+            prijs.append(8)
+    return prijs
 
 
 def main():
     activiteiten = ["Atletiek", "Batting Cage", "Boksen", "Biatlon", "Big game fishing", "Bouldering", "Cliff Jump",
-                "Core Stability", "Curling", "Dansen", "Darts", "E-Ziplining", "Iggy", "Lopen", "Golf", "Hexatech",
-                "Multiball", "Neo stadium", "Boogschieten", "ParaGliding", "Parkours", "Racen", "Roeien", "Schaatsen",
-                "Shooting range", "Skiën", "Slackline", "SnowBoarden", "PXspeL Arena", "Speed Climbing",
-                "Suspended nets", "Trampoline", "Vissen", "Wielrennen", "Voetbal", "Vrije val", "Wingsuit"]
+                    "Core Stability", "Curling", "Dansen", "Darts", "E-Ziplining", "Iggy", "Lopen", "Golf", "Hexatech",
+                    "Multiball", "Neo stadium", "Boogschieten", "ParaGliding", "Parkours", "Racen", "Roeien",
+                    "Schaatsen",
+                    "Shooting range", "Skiën", "Slackline", "SnowBoarden", "PXspeL Arena", "Speed Climbing",
+                    "Suspended nets", "Trampoline", "Vissen", "Wielrennen", "Voetbal", "Vrije val", "Wingsuit"]
 
     # 1)
     naam = input("Geef je naam: ")
-
+    while len(naam) < 3:
+        naam = input("Geef je naam: ")
 
     # 2)
     code = int(input("Geef een 5-cijferige code voor de locker: "))
+    while len(str(code)) < 5:
+        code = int(input("Geef een 5-cijferige code voor de locker: "))
+
+    geslecteerdeActiviteit = selecteer_activiteiten(activiteiten, naam)
+    prijs = bepaal_prijs_geselecteerde_activiteiten(geslecteerdeActiviteit, code)
 
     print("\nLijst activteiten")
-
+    for i in range(len(geslecteerdeActiviteit)):
+        print(f"{geslecteerdeActiviteit[i]} {prijs[i]}")
     # 3)
+    lijstMetActiviteiten = []
     woord = input("\nMaak een keuze uit de lijst van activiteiten. 'Stop' om te stoppen.\n")
+    lijstMetActiviteiten.append(woord)
+    while woord.lower() != "stop":
+        woord = input()
+        lijstMetActiviteiten.append(woord)
+
+    te_betalen(lijstMetActiviteiten, geslecteerdeActiviteit, prijs)
+
+
+def te_betalen(lijstMetActiviteiten, geselecteerdeActiviteit, prijs):
+    betalen = 0
+    for i in range(len(geselecteerdeActiviteit)):
+        for j in range(len(lijstMetActiviteiten)):
+            if geselecteerdeActiviteit[i].lower() == lijstMetActiviteiten[j].lower():
+                betalen += prijs[i]
+    print(f"Te betalen: {betalen}")
 
 
 if __name__ == '__main__':
