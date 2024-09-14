620fe234055c4c683ea9f785bb520f358c58bb0d
profyx-ater <65975213+profyx-ater@users.noreply.github.com>
2024-09-14 17:53:30 +0200

    vraag 1 en 2

---
+++ vraag1.py
@@ -0,0 +1,8 @@
+
+def main():
+    maanden = ['Januari;31', 'Februari;29', 'Maart;31', 'April;30', 'Mei;31', 'Juni;30',
+               'Juli;31','Augustus;31', 'September;30', 'Oktober;31', 'November;30', 'December;31']
+
+
+if __name__ == '__main__':
+    main()
---
+++ vraag2.py
@@ -0,0 +1,30 @@
+# def selecteer_activiteiten():
+
+
+
+# def bepaal_prijs_geselecteerde_activiteiten():
+
+
+
+def main():
+    activiteiten = ["Atletiek", "Batting Cage", "Boksen", "Biatlon", "Big game fishing", "Bouldering", "Cliff Jump",
+                "Core Stability", "Curling", "Dansen", "Darts", "E-Ziplining", "Iggy", "Lopen", "Golf", "Hexatech",
+                "Multiball", "Neo stadium", "Boogschieten", "ParaGliding", "Parkours", "Racen", "Roeien", "Schaatsen",
+                "Shooting range", "Skiën", "Slackline", "SnowBoarden", "PXspeL Arena", "Speed Climbing",
+                "Suspended nets", "Trampoline", "Vissen", "Wielrennen", "Voetbal", "Vrije val", "Wingsuit"]
+
+    # 1)
+    naam = input("Geef je naam: ")
+
+
+    # 2)
+    code = int(input("Geef een 5-cijferige code voor de locker: "))
+
+    print("\nLijst activteiten")
+
+    # 3)
+    woord = input("\nMaak een keuze uit de lijst van activiteiten. 'Stop' om te stoppen.\n")
+
+
+if __name__ == '__main__':
+    main()
